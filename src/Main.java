e6f35460f02e870eb649c9ccd3d68d6683811f2b
Hergli-omar <hergliomarh@gmail.com>
2026-02-24 01:11:33 +0100

    Prosit 3

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,43 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        //        Animal lion = new Animal();
-        //        Zoo myZoo = new Zoo();
-        //        lion.family="Felidae";
-        //        lion.name="Lion";
-        //        lion.age=8;
-        //        myZoo.name="MyZoo";
-        //        myZoo.city="Paris";
-        //        myZoo.nbrCages=10;
+//        Animal lion = new Animal();
+//        Zoo myZoo = new Zoo();
+//        lion.family="Felidae";
+//        lion.name="Lion";
+//        lion.age=8;
+//        myZoo.name="MyZoo";
+//        myZoo.city="Paris";
+//        myZoo.nbrCages=10;
         Animal lion = new Animal("Felidae", "Lion", 8, true);
-        Zoo myZoo = new Zoo("MyZoo", "Paris", 10);
+        Zoo myZoo = new Zoo("MyZoo", "Paris");
         Animal a1 = new Animal("Elephantidae", "Elephant", 15, true);
         Animal a2 = new Animal("Crocodylidae", "Crocodile", 20, false);
         Animal a3 = new Animal("Ursidae", "Panda", 5, true);
-        Zoo z1 = new Zoo("New York Zoo", "New York", 30);
-        Zoo z2 = new Zoo("Tokyo Zoo", "Tokyo", 15);
-        Zoo z3 = new Zoo("London Zoo", "London", 25);
+        Zoo z1 = new Zoo("New York Zoo", "New York");
+        Zoo z2 = new Zoo("Tokyo Zoo", "Tokyo");
+        Zoo z3 = new Zoo("London Zoo", "London");
         //z1.displayZoo();
-        System.out.println(z1);
-        System.out.println(z1.toString()) ;
-        System.out.println(lion);
+//        System.out.println(z1);
+//        System.out.println(z1.toString()) ;
+//        System.out.println(lion);
+
+        myZoo.addAnimal(lion);
+        myZoo.addAnimal(a1);
+        myZoo.addAnimal(a2);
+        myZoo.addAnimal(a3);
+
+        myZoo.afficheAnimals();
+        System.out.println(myZoo.searchAnimal(a2));
+        myZoo.removeAnimal(a2);
+        myZoo.afficheAnimals();
+
+        System.out.println(myZoo.isZooFull());
+
+
+
+
 
     }
 }
--- src/Zoo.java
+++ src/Zoo.java
@@ -1,18 +1,19 @@
 import java.util.Arrays;
 
 public class Zoo {
+
     Animal[] animals;
     String name;
     String city;
-    int nbrCages;
+    final int  nbrCages=25;
+    static int compteur;
     public Zoo() {
-        animals = new Animal[25];
+        animals = new Animal[nbrCages];
     }
-    public Zoo(String name, String city, int nbrCages) {
-        animals = new Animal[25];
+    public Zoo(String name, String city) {
+        animals = new Animal[nbrCages];
         this.name = name;
         this.city = city;
-        this.nbrCages = nbrCages;
     }
     public void displayZoo(){
         System.out.println("Zoo name: "+name);
@@ -22,5 +23,61 @@ public class Zoo {
     public String toString(){
         return "Zoo name: "+name +"Zoo city: "+city +"Zoo nbr cages: "+nbrCages;
     }
+
+    public boolean addAnimal(Animal animal){
+        if (this.searchAnimal(animal)!=-1) return false;
+        else {
+            if (compteur < animals.length) {
+                animals[compteur] = animal;
+                compteur++;
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+    public void afficheAnimals(){
+        for(int i=0; i<compteur; i++){
+            System.out.println(animals[i]);
+        }
+    }
+    public int searchAnimal(Animal animal){
+        int i=0;
+        while(i<compteur){
+            if(animals[i].name==animal.name){
+                return i;
+            }
+            i++;
+        }
+        return -1;
+    }
+    public boolean removeAnimal(Animal animal){
+        int i=0;
+        if (this.searchAnimal(animal)==-1) return false;
+        else {
+            i=this.searchAnimal(animal);
+            for (int j=i; j<compteur; j++){
+                animals[j]=animals[j+1];
+            }
+            compteur--;
+            return true;
+        }
+
+    }
+    public boolean isZooFull(){
+        if(compteur<animals.length){
+            return false;
+        }
+        return true;
+    }
+    public Zoo comparerZoo(Zoo z1, Zoo z2){
+        if (z1.compteur >z2.compteur){
+            return z1;
+        }
+        else{
+            return z2;
+        }
+    }
+
 
 }
